3d5c8c9a22667846aa052ee45e9f84a7a3c61eaa
Luca <lfdaloia@ncsu.edu>
2025-06-10 08:59:23 -0400

    'pdf_parsing.py' pulls data from a list of file paths

--- Figure_of_Merit-main/FOM_project/my_main_app/pdf_parsing.py
+++ Figure_of_Merit-main/FOM_project/my_main_app/pdf_parsing.py
@@ -3,7 +3,8 @@ import PyPDF2 #library to read and extract data/text from PDF document
 import requests
 from urllib.parse import quote
 
-global doi 
+global doi, dois
+dois = []
 
 # function to extract data from pdf
 def pdf_doi_extraction(file_path):
@@ -21,11 +22,22 @@ def pdf_doi_extraction(file_path):
         print('DOI not found')
     return doi
 
+
+
 # File path variable - change to GUI to allow user to select multiple from file explorer
-file_path = r'C:\Users\Luca\Downloads\Figure_of_Merit-main\Figure_of_Merit-main\FOM_project\my_main_app\Evolution_Challenges_and_Applications_of_Modern_MO.pdf'
-doi = pdf_doi_extraction(file_path)
-print('DOI:',doi)
-doi_encoded = quote(str(doi), safe='')
+
+file_paths = [r'C:\Users\Luca\Downloads\Figure_of_Merit-main\Figure_of_Merit-main\FOM_project\my_main_app\Evolution_Challenges_and_Applications_of_Modern_MO.pdf', 
+              r'C:\Users\Luca\Downloads\Figure_of_Merit-main\Figure_of_Merit-main\FOM_project\my_main_app\Applications_and_challenges_of_Silicon_Carbide_SiC.pdf',
+              r'C:\Users\Luca\Downloads\Figure_of_Merit-main\Figure_of_Merit-main\FOM_project\my_main_app\10.1109-16.536820.pdf']
+
+for file_path in file_paths:
+    doi = pdf_doi_extraction(file_path)
+    #print('DOI:',doi)
+    doi_encoded = quote(str(doi), safe='')
+    dois.append(doi_encoded)
+
+print(dois)
+
 
 global title, author, journal, year
 title = None
@@ -33,47 +45,52 @@ author = None
 journal = None
 year = None
 
-def file_data_extraction(doi):
-    url = f"https://api.crossref.org/works/{doi}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        metadata = data['message']
-        # get title
-        title = metadata.get('title', [None])[0]
 
-        # get authors and add them to a string (list separated by comma)
-        authors = metadata.get('author', []) 
-        authors_list = []
-        for author in authors:
-            # Combine given and family names, if available
-            name_parts = []
-            if 'given' in author:
-                name_parts.append(author['given'])
-            if 'family' in author:
-                name_parts.append(author['family'])
-            full_name = ' '.join(name_parts)
-            authors_list.append(full_name)
-    
-        authors_string = ', '.join(authors_list)
-        author = authors_string
 
-        # Get Journal
-        journal = metadata.get('container-title', [None])[0]
+def file_data_extraction(dois):
+    for doi in dois:
+        url = f"https://api.crossref.org/works/{doi}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            metadata = data['message']
+            # get title
+            title = metadata.get('title', [None])[0]
 
-        # Get year
-        year = metadata.get('issued', {}).get('date-parts', [[None]])[0][0]
+            # get authors and add them to a string (list separated by comma)
+            authors = metadata.get('author', []) 
+            authors_list = []
+            for author in authors:
+                # Combine given and family names, if available
+                name_parts = []
+                if 'given' in author:
+                    name_parts.append(author['given'])
+                if 'family' in author:
+                    name_parts.append(author['family'])
+                full_name = ' '.join(name_parts)
+                authors_list.append(full_name)
+        
+            authors_string = ', '.join(authors_list)
+            author = authors_string
 
-        print('Title:', title,
-            '\nAuthor:', author,
-            '\nJournal:', journal,
-            '\nYear:', year)
+            # Get Journal
+            journal = metadata.get('container-title', [None])[0]
+
+            # Get year
+            year = metadata.get('issued', {}).get('date-parts', [[None]])[0][0]
+
+            print('DOI:', doi,
+                '\nTitle:', title,
+                '\nAuthor:', author,
+                '\nJournal:', journal,
+                '\nYear:', year,
+                '\n------------------------------\n')
 
     else:
         print("DOI not found or error occurred.")
 
 
-file_data_extraction(doi_encoded)
+file_data_extraction(dois)
 
 
 
---
+++ Figure_of_Merit-main/FOM_project/my_main_app/pdf_select.py
@@ -0,0 +1,3 @@
+# GUI to select PDFs to be parsed
+# function will return a list of the paths to the selected files
+
